f27c1235708b5bf231c0094a1eb0febae0a3f6ed
Sergio Aliceral <sergio.aliceral@sesicni.com.br>
2021-03-16 06:54:17 -0300

    first commit

---
+++ src/main/java/br/com/aliceraltecnologia/HelloActuatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.aliceraltecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloActuatorApplication.class, args);
+	}
+
+}
